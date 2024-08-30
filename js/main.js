dd9e36e23db311e25304acc40a906440ebd9fee9
mohamadreza <mohamadreza1388.org@gmail.com>
2024-08-30 19:41:02 +0330

    refactore(clean): clean code

--- js/main.js
+++ js/main.js
@@ -5,24 +5,23 @@
 * Version: 1.0
 */
 
-(function($) {
-"use strict";
+(function ($) {
+    "use strict";
+
     // Subpages resize
     function subpages_resize() {
         var subpagesHeight = $('.pt-page-current').height();
         $(".subpages").height(subpagesHeight + 50);
     }
-    
+
     // Portfolio subpage filters
     function portfolio_init() {
-        var portfolio_grid = $('#portfolio_grid'),
-            portfolio_filter = $('#portfolio_filters');
-            
+        var portfolio_grid = $('#portfolio_grid'), portfolio_filter = $('#portfolio_filters');
+
         if (portfolio_grid) {
 
             portfolio_grid.shuffle({
-                speed: 450,
-                itemSelector: 'figure'
+                speed: 450, itemSelector: 'figure'
             });
 
             $('.site-main-menu').on("click", "a", function (e) {
@@ -35,14 +34,15 @@
                 e.preventDefault();
                 $('#portfolio_filters .filter').parent().removeClass('active');
                 $(this).parent().addClass('active');
-                portfolio_grid.shuffle('shuffle', $(this).attr('data-group') );
-                setTimeout(function(){
+                portfolio_grid.shuffle('shuffle', $(this).attr('data-group'));
+                setTimeout(function () {
                     subpages_resize();
                 }, 500);
             });
 
         }
     }
+
     // /Portfolio subpage filters
 
     // Contact form validator
@@ -55,11 +55,7 @@
                 var url = "contact_form/contact_form.php";
 
                 $.ajax({
-                    type: "POST",
-                    url: url,
-                    data: $(this).serialize(),
-                    success: function (data)
-                    {
+                    type: "POST", url: url, data: $(this).serialize(), success: function (data) {
                         var messageAlert = 'alert-' + data.type;
                         var messageText = data.message;
 
@@ -83,11 +79,12 @@
             $('#site_header').addClass('mobile-menu-hide');
         }
     }
+
     // /Hide Mobile menu
 
     //On Window load & Resize
     $(window)
-        .on('load', function() { //Load
+        .on('load', function () { //Load
             // Animation on Page Loading
             $(".preloader").fadeOut("slow");
 
@@ -99,10 +96,10 @@
                 });
             }
         })
-        .on('resize', function() { //Resize
-             mobileMenuHide();
+        .on('resize', function () { //Resize
+            mobileMenuHide();
 
-             setTimeout(function(){
+            setTimeout(function () {
                 subpages_resize();
             }, 500);
         })
@@ -117,21 +114,23 @@
 
 
     // On Document Load
-    $(document).on('ready', function() {
+    $(document).on('ready', function () {
         // Initialize Portfolio grid
         var $portfolio_container = $("#portfolio-grid");
 
         $portfolio_container.imagesLoaded(function () {
-            setTimeout(function(){
+            setTimeout(function () {
                 portfolio_init(this);
             }, 500);
         });
 
         // Portfolio hover effect init
-        $(' #portfolio_grid > figure ').each( function() { $(this).hoverdir(); } );
+        $(' #portfolio_grid > figure ').each(function () {
+            $(this).hoverdir();
+        });
 
         // Blog grid init
-        setTimeout(function(){
+        setTimeout(function () {
             var $container = $(".blog-masonry");
             $container.masonry();
         }, 500);
@@ -156,22 +155,17 @@
             nav: true, // Show next/prev buttons.
             items: 3, // The number of items you want to see on the screen.
             loop: false, // Infinity loop. Duplicate last and first items to get loop illusion.
-            navText: false,
-            margin: 25,
-            responsive : {
+            navText: false, margin: 25, responsive: {
                 // breakpoint from 0 up
-                0 : {
+                0: {
                     items: 1,
-                },
-                // breakpoint from 480 up
-                480 : {
+                }, // breakpoint from 480 up
+                480: {
                     items: 1,
-                },
-                // breakpoint from 768 up
-                768 : {
+                }, // breakpoint from 768 up
+                768: {
                     items: 2,
-                },
-                1200 : {
+                }, 1200: {
                     items: 2,
                 }
             }
@@ -194,47 +188,34 @@
 
         // Lightbox init
         $('body').magnificPopup({
-            delegate: 'a.lightbox',
-            type: 'image',
-            removalDelay: 300,
+            delegate: 'a.lightbox', type: 'image', removalDelay: 300,
 
             // Class that is added to popup wrapper and background
             // make it unique to apply your CSS animations just to this exact popup
-            mainClass: 'mfp-fade',
-            image: {
+            mainClass: 'mfp-fade', image: {
                 // options for image content type
-                titleSrc: 'title',
-                gallery: {
+                titleSrc: 'title', gallery: {
                     enabled: true
                 },
             },
 
             iframe: {
-                markup: '<div class="mfp-iframe-scaler">'+
-                        '<div class="mfp-close"></div>'+
-                        '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-                        '<div class="mfp-title mfp-bottom-iframe-title"></div>'+
-                      '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
+                markup: '<div class="mfp-iframe-scaler">' + '<div class="mfp-close"></div>' + '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' + '<div class="mfp-title mfp-bottom-iframe-title"></div>' + '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
 
                 patterns: {
                     youtube: {
-                      index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
+                        index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
 
-                      id: null, // String that splits URL in a two parts, second part should be %id%
-                      // Or null - full URL will be returned
-                      // Or a function that should return %id%, for example:
-                      // id: function(url) { return 'parsed id'; }
+                        id: null, // String that splits URL in a two parts, second part should be %id%
+                        // Or null - full URL will be returned
+                        // Or a function that should return %id%, for example:
+                        // id: function(url) { return 'parsed id'; }
 
-                      src: '%id%?autoplay=1' // URL that will be set as a source for iframe.
-                    },
-                    vimeo: {
-                      index: 'vimeo.com/',
-                      id: '/',
-                      src: '//player.vimeo.com/video/%id%?autoplay=1'
-                    },
-                    gmaps: {
-                      index: '//maps.google.',
-                      src: '%id%&output=embed'
+                        src: '%id%?autoplay=1' // URL that will be set as a source for iframe.
+                    }, vimeo: {
+                        index: 'vimeo.com/', id: '/', src: '//player.vimeo.com/video/%id%?autoplay=1'
+                    }, gmaps: {
+                        index: '//maps.google.', src: '%id%&output=embed'
                     }
                 },
 
@@ -242,17 +223,14 @@
             },
 
             callbacks: {
-                markupParse: function(template, values, item) {
-                 values.title = item.el.attr('title');
+                markupParse: function (template, values, item) {
+                    values.title = item.el.attr('title');
                 }
             },
         });
 
         $('.ajax-page-load-link').magnificPopup({
-            type: 'ajax',
-            removalDelay: 300,
-            mainClass: 'mfp-fade',
-            gallery: {
+            type: 'ajax', removalDelay: 300, mainClass: 'mfp-fade', gallery: {
                 enabled: true
             },
         });
@@ -260,11 +238,11 @@
         //Form Controls
         $('.form-control')
             .val('')
-            .on("focusin", function(){
+            .on("focusin", function () {
                 $(this).parent('.form-group').addClass('form-group-focus');
             })
-            .on("focusout", function(){
-                if($(this).val().length === 0) {
+            .on("focusout", function () {
+                if ($(this).val().length === 0) {
                     $(this).parent('.form-group').removeClass('form-group-focus');
                 }
             });
